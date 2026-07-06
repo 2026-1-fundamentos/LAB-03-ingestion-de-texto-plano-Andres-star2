b4ec11402d57a6b99c2a6bd52e7ee2c028115460
Andres-star1 <androjasgu@unal.edu.co>
2026-07-06 14:16:46 -0500

    inyenyto 3

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,7 +4,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
-
+import pandas as pd
 def pregunta_01():
     """
     Construya y retorne un dataframe de Pandas a partir del archivo
@@ -18,3 +18,42 @@ def pregunta_01():
 
 
     """
+    with open("files/input/clusters_report.txt", encoding="utf-8") as file:
+        datos = []
+        for _ in range(4):
+            next(file)
+
+        cluster = ""
+        cantidad = ""
+        procentaje = " "
+        palabras = ""
+
+        for linea in file:
+            if linea.strip() == "":
+                continue
+            
+            primera = linea.strip().split()[0]
+
+            if primera.isdigit():
+                if cluster !="":
+                    palabras = " ".join(palabras.split()).rstrip(".")
+                    datos.append([int(cluster),int(cantidad),porcentaje,palabras])
+
+                partes = linea.split()
+
+                cluster = partes[0]
+                cantidad = partes[1]
+                porcentaje = float(partes[2].replace(",","."))
+                palabras = " ".join(partes[4:])
+
+            else:
+                palabras += " "+" ".join(linea.split()).rstrip(".")
+        datos.append([int(cluster),int(cantidad),porcentaje,palabras])   
+
+        df = pd.DataFrame(datos,columns=["cluster","cantidad_de_palabras_clave","porcentaje_de_palabras_clave","principales_palabras_clave",])    
+
+    return df
+print(pregunta_01())
+
+
+
